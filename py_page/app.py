24b5aed613cfd52bfb61755b6daaa232128b5f47
NEXIRO055 <lsmcreate055@gmail.com>
2025-01-11 13:52:18 +0900

    streamlit_test

---
+++ py_page/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from time import sleep
+
+st.set_page_config(
+    page_icon="🎺",
+    page_title="Streamlit App",
+    layout="wide"
+)
+
+st.header("Streamlit App")
+st.subheader("This is a subheader")
